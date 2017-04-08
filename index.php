64f3a48b7a8307b8ccb06d038401dff1dd41955a
Stoian Catalin <shaggy.cata@gmail.com>
2017-04-08 15:55:18 +0300

    Add php structure

--- index.php
+++ index.php
@@ -1,49 +1,9 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Kimo - Kido Raddar</title>
-    <link rel="stylesheet" href="semantic/semantic.min.css" />
-    <link rel="stylesheet" href="styles/main.css" />
-</head>
-<body>
+<?php include_once('parts/header.php') ?>
 <div class="ui sidebar inverted vertical menu">
-    <h1 class="logo">
-        <i class="icon street view"></i>Kimo
-        <span>Safe. Watch. Tehnology.</span>
-    </h1>
-    <a class="item">
-        <i class="icon home"></i>
-        Home
-    </a>
-    <a class="item">
-        <i class="icon info"></i>
-        About
-    </a>
-    <a class="item">
-        <i class="icon legal"></i>
-        Terms and Conditions
-    </a>
+    <?php include_once('parts/sidebar.php'); ?>
 </div>
 <div class="pusher">
-    <nav class="topbar">
-        <div class="container-flex">
-            <div class="open-menu">
-                <i class="icon content"></i>
-            </div>
-            <h1 class="logo">
-                <i class="icon street view"></i>Kimo
-                <span>Safe. Watch. Tehnology.</span>
-            </h1>
-            <div class="actions">
-                <div class="logout ui icon button circular">
-                    <i class="icon setting"></i>
-                </div>
-            </div>
-        </div>
-    </nav>
-
+    <?php include_once('parts/topbar.php') ?>
     <div class="content">
         <div class="card">
             <h2 class="ui center aligned icon header">
@@ -59,17 +19,6 @@
             </div>
         </div>
     </div>
-
-    <footer>
-        Copyright &copy; Kimo - FII TW <i class="icon smile"></i>
-    </footer>
+    <?php include_once('parts/footer.php') ?>
 </div>
-
-<script
-        src="https://code.jquery.com/jquery-3.1.1.min.js"
-        integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8="
-        crossorigin="anonymous"></script>
-<script src="semantic/semantic.min.js"></script>
-<script src="scripts/scripts.js"></script>
-</body>
-</html>
+<?php include_once('parts/end.php') ?>
